9a2d413c7e5bea301a4a23a39867fe1828a70593
Ankit Sharma <ankitsharm877@gmail.com>
2020-08-27 20:33:28 +0530

    new

---
+++ form-array-example/src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormControl,FormArray, FormBuilder } from '@angular/forms'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'form-array-example';
+
+  skillsForm: FormGroup;
+ 
+  constructor(private fb:FormBuilder) {
+ 
+    this.skillsForm = this.fb.group({
+      name: '',
+      skills: this.fb.array([]) ,
+    });
+  
+  }
+ 
+  skills() : FormArray {
+    return this.skillsForm.get("skills") as FormArray;
+  }
+ 
+  newSkill(): FormGroup {
+    return this.fb.group({
+      skill: '',
+      exp: '',
+    })
+  }
+ 
+  addSkills() {
+    this.skills().push(this.newSkill());
+  }
+ 
+  removeSkill(i:number) {
+    this.skills().removeAt(i);
+  }
+ 
+  onSubmit() {
+    console.log(this.skillsForm.value);
+  }
+ 
+}
+ 
+ 
+export class country {
+  id: string;
+  name: string;
+ 
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+}
