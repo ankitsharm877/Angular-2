c0712af75dd7cfcc3e9f6c6d1b30e036280bec7b
Ankit Sharma <ankitsharm877@gmail.com>
2020-08-27 22:04:23 +0530

    new

---
+++ template-driven-forms-example/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent{
+  title = 'Template driven forms';
+ 
+  countryList:country[] = [
+    new country("1", "India"),
+    new country('2', 'USA'),
+    new country('3', 'England')
+  ];
+
+  onSubmit(contactForm) {
+    console.log(contactForm.value);
+  }
+}
+ 
+export class country {
+  id:string;
+  name:string;
+ 
+  constructor(id:string, name:string) {
+    this.id=id;
+    this.name=name;
+  }
+}
