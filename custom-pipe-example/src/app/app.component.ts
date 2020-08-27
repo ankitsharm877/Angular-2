b003c0d85c6fd90f144e897c086032ffded351d1
Ankit Sharma <ankitsharm877@gmail.com>
2020-08-27 19:59:07 +0530

    new

---
+++ custom-pipe-example/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'custom-pipe-example';
+  sampleString: string ='Welcome to angular 2';
+}
