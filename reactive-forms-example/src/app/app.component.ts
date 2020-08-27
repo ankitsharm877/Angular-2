20ae2eab583a5d62634e8b4b7f7f0f6d7eaff34d
Ankit Sharma <ankitsharm877@gmail.com>
2020-08-27 20:53:15 +0530

    new

---
+++ reactive-forms-example/src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { FormGroup,FormControl, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'reactive-forms-example';
+ 
+  countryList: country[] = [
+    new country("1", "India"),
+    new country('2', 'USA'),
+    new country('3', 'England')
+  ];
+
+  contactForm = new FormGroup({
+    firstname: new FormControl(),
+    lastname: new FormControl(),
+    email: new FormControl(),
+    gender: new FormControl(),
+    isMarried: new FormControl(),
+    country: new FormControl(),
+    address:new FormGroup({
+      city: new FormControl(),
+      street: new FormControl(),
+      pincode:new FormControl()
+    })
+  })
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
+}
+export class contact {
+  firstname: string;
+  lastname: string;
+  email: string;
+  gender: string;
+  isMarried: boolean;
+  country: string;
+  address: {
+    city: string;
+    street: string;
+    pincode: string;
+  }
+}
+ 
+ 
+export class country {
+  id: string;
+  name: string;
+ 
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+}
